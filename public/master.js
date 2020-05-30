f7b916e1ac483bbf212b3a1b29614d13c012e677
fructu8 <mfructuosob@gmail.com>
2020-05-30 19:03:15 +0100

    afegit forma d'ona

--- public/master.js
+++ public/master.js
@@ -1,17 +1,18 @@
 //Make makeconnection
-var socket = io.connect('http://localhost:4000')
+var socket = io.connect('http://localhost:4000');
+var forma = "sine";
 
 
   $("button").on("click", function(){
-	  updateSound($(this).val());
 
+    updateSound($(this).val(), $("#forma").val());
   });
 
 
-  function updateSound (frequencia){
+  function updateSound (frequencia, forma){
 
 	  socket.emit('channel', { //emitting a message called "channel" with 2 parameters"
 		  frequency: frequencia, //parameter 1
-		  type: 'sine' //parameter 2
+		  type: forma, //parameter 2
 	  });
   }
