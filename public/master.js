aa4f037c36ecdf095c64b90a656f34076720e12f
fructu8 <mfructuosob@gmail.com>
2020-07-01 12:31:10 +0100

    stop on release

--- public/master.js
+++ public/master.js
@@ -21,20 +21,46 @@ var forma = "sine";
   }
 
 $("body").on("keypress", function(e){
-
-
-
     var code = e.keyCode || e.which;
-    alert(code);
-
     switch (code){
         case (97):
             updateSound(261.63, $("#forma").val());
             break;
-        case (114):
-            updateSound(261.63, $("#forma").val());
+        case (119):
+            updateSound(277.18, $("#forma").val());
+            break;
+        case (115):
+            updateSound(293.66, $("#forma").val());
+            break;
+        case (101):
+            updateSound(311.13, $("#forma").val());
+            break;
+        case (100):
+            updateSound(329.63, $("#forma").val());
+            break;
+        case (102):
+            updateSound(349.23, $("#forma").val());
+            break;
+        case (116):
+            updateSound(369.99, $("#forma").val());
+            break;
+        case (103):
+            updateSound(392.00, $("#forma").val());
+            break;
+        case (121):
+            updateSound(415.30, $("#forma").val());
+            break;
+        case (104):
+            updateSound(440.00, $("#forma").val());
+            break;
+        case (117):
+            updateSound(466.16, $("#forma").val());
+            break;
+        case (106):
+            updateSound(493.88, $("#forma").val());
             break;
     }
-
-
+});
+$("body").on("keyup", function(e){
+updateSound(0, $("#forma").val());
 });
